c5e54849f287801bd2c0da774bf618254945234b
HenriqueFreire <henriquefreire@outlook.com.br>
2024-03-03 07:58:32 -0300

    Aula 113 | exercício 7 concluída

---
+++ Curso de web com JavaScript/6-Javascript: Funções/113-exercicios/03-exec.js
@@ -0,0 +1,9 @@
+function expoente(base, expoente){
+  // Podemos fazer de 2 modos
+  // Método antigo:
+  let resultado = Math.pow(base, expoente)
+  // Método novo
+  resultado = base ** expoente
+}
+
+console.log(expoente(2,3))
---
+++ Curso de web com JavaScript/6-Javascript: Funções/113-exercicios/07-exec.js
@@ -0,0 +1,8 @@
+function bhaskara (ax2, bx, c){
+  let resultados = []
+  let delta = (bx ** 2) - (4* ax2 * c)
+
+  if (delta < 0) {
+    return 'Delta é negativo'
+  }
+}
