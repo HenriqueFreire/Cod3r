4271d09f93ff116db9b5ac94dae22892a8839271
HenriqueFreire <henriquefreire@outlook.com.br>
2024-03-03 18:53:49 -0300

    Aula 113 | exercício 35 concluída

---
+++ Curso de web com JavaScript/6-Javascript: Funções/113-exercicios/25-exec.js
@@ -0,0 +1,7 @@
+function contador() {
+  for (let i = 0; i < array.length; i++) {
+    console.log(i)    
+  }
+}
+
+contador()
---
+++ Curso de web com JavaScript/6-Javascript: Funções/113-exercicios/26-exec.js
@@ -0,0 +1,9 @@
+function acharPares() {
+  for (let i = 0; i < array.length; i++) {
+    if (i % 2 == 0) {
+      console.log(i)
+    }    
+  }
+}
+
+acharPares()
